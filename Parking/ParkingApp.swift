18fcd252015a4f7a13479c21c316665c9f5b2573
myname <myemail@myemail.com>
2023-02-28 07:33:35 +0800

    Initial Commit

---
+++ Parking/ParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingApp.swift
+//  Parking
+//
+//  Created by stdc user on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
